f4e5d9f0b27a8e7f8a180b452303a65d77c6c009
Honwei <ayhwtian@163.com>
2020-06-16 16:13:56 +0800

    '添加了删除全部笔记功能，根据有道云笔记优化了新建笔记功能，基本功能测试都没有问题，改项目到此结束'

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,31 @@
   <div id="app">
     <div class="content">
       <div class="side-bar">
-        <div class="btn"><button @click="newNote" :title="showBtnTitle">新建笔记</button></div>
+        <div class="new-btn"><button @click="newNote" :title="showBtnTitle">新建笔记</button></div>
         <div class="list"
              v-for="note in notes"
-             @click="selectNote(note)" :class="{selected:note === selectedNote}">{{note.title}}</div>
+             @click="selectNote(note)" :class="{selected:note === selectedNote}"><div class="font">{{note.title}}</div></div>
       </div>
       <div class="edit-panel">
-        <input type="text"
-               value="无标题笔记"
-               onfocus="if(value == '无标题笔记' ){value=''}"
-               onblur="if(value == '' ){value='无标题笔记'}"
-               ref="title"
-               @input="updateTitle" maxlength="16">
-        <button @click="removeNote">删除</button>
-        <textarea id="txt-area"
-                  v-model="content"
-                  @input="updateContent">{{content}}</textarea>
+        <div class="edit-bar">
+          <input type="text"
+                 value="无标题笔记"
+                 onfocus="if(value == '无标题笔记' ){value=''}"
+                 onblur="if(value == '' ){value='无标题笔记'}"
+                 ref="title"
+                 @input="updateTitle">
+          <button class="del-btn" @click="removeNote">删除选中笔记</button>
+          <button class="delall-btn" @click="removeAllNote">清空笔记</button>
+        </div>
+        <div class="txt-edit">
+          <div class="line"><div v-for="i in linesCount">{{i}}</div></div>
+          <textarea id="txt-area"
+                    v-model="content"
+                    @input="updateContent">{{content}}</textarea>
+        </div>
       </div>
       <div class="show-panel" v-html="notePreview">{{notePreview}}</div>
-    </div>i
-    <footer>脚</footer>
+    </div>
   </div>
 </template>
 
@@ -36,7 +41,8 @@
         content: '',
         notes: JSON.parse(localStorage.getItem('notes')) || [],
         selectedID: localStorage.getItem('selected-id') || null,
-        title: '',
+        title: '无标题笔记',
+        isShow: true,
       }
     },
     computed: {
@@ -54,8 +60,26 @@
       //  返回与选择ID一样的笔记内容
         return this.notes.find(note => note.id === this.selectedID)
       },
+      //计算行数
+      linesCount() {
+        if (this.selectedNote) {
+          // 计算换行符的个数
+          // return this.selectedNote.content.split(/\r\n|\r|\n/).length
+          return this.content.split(/\r\n|\r|\n/).length
+        }
+      },
 
     },
+    mounted() {
+      if (this.selectedNote) {
+      this.content = this.selectedNote.content;
+      }
+      if(localStorage.getItem('selected-id') == 'null'){
+        this.selectedID = null
+      }
+
+    }
+    ,
     watch: {
       notes: {
         handler:'saveNotes',
@@ -70,21 +94,29 @@
       //保存笔记列表
       saveNotes() {
         localStorage.setItem('notes', JSON.stringify(this.notes))
-        console.log('笔记已保存' + new Date);
+        // console.log('笔记已保存' + new Date);
       },
       //新建笔记：如果当前有笔记的话，保存当前笔记，清空标题和内容
       newNote() {
         //  新笔记默认值
         const note = {
                 id: String(Date.now()),
-                title: this.$refs.title.value,
-                content:this.content,
+                title: '无标题笔记',
+                content:'',
                 created: Date.now(),
-                favorite: false
         }
 
+        if(this.selectedID === null) {
+          note.title = this.$refs.title.value
+          note.content = this.content
+        }
         // 添加到列表中
         this.notes.push(note)
+
+        this.selectedID = note.id
+        this.$refs.title.value = '无标题笔记'
+        this.content = ''
+        this.notes.sort((a, b) => b.created - a.created)
       },
 
       //选中了那条笔记的ID，用此ID来选择笔记内容
@@ -95,22 +127,36 @@
       },
       //修改title
       updateTitle() {
+        if(this.selectedID !== null) {
         return this.selectedNote.title = this.$refs.title.value
+        }
       },
       //  修改内容
       updateContent() {
+        if(this.selectedID !== null) {
         return this.selectedNote.content = this.content
+        }
       },
       //删除笔记
       removeNote() {
-        if (this.selectedNote && confirm('Delete the note?')) {
+        if (this.selectedNote && confirm('删除后不可恢复，确定要删除当前笔记吗')) {
           // 将选中的笔记从笔记列表中移除
           const index = this.notes.indexOf(this.selectedNote)
           if (index !== -1) {
             this.notes.splice(index, 1)
           }
           this.$refs.title.value = this.title
-
+          this.content = ''
+          this.selectedID = null
+        }
+      },
+      removeAllNote() {
+        if (confirm('笔记清空后不可恢复，确定要清空吗？')) {
+          // 将选中的笔记从笔记列表中移除
+          this.notes.splice(0, this.notes.length)
+          this.$refs.title.value = this.title
+          this.content = ''
+          this.selectedID = null
         }
       }
     },
@@ -120,77 +166,5 @@
 </script>
 
 <style lang="less">
-    html,body {
-        margin: 0;
-        padding: 0;
-        height: 100%;
-    }
-    #app {
-      display: -webkit-flex; /*Safari*/
-      display: flex;
-      flex-direction: column;
-      height: 100%;
-    }
-
-
-    .content {
-      display: flex;
-      height: 100%;
-      margin-top: 5px;
-    }
-
-    .side-bar {
-      display: flex;
-      width: 15%;
-      flex-direction: column;
-      /*justify-content: center;*/
-      align-items: center;
-    }
-
-    .btn {
-      text-align: center;
-      width: 100%;
-      background-color: #0985db;
-    }
-    .btn button {
-      border: none;
-      height: 30px;
-      width: 100%;
-      background-color: transparent;
-      color: #FFFFFF;
-    }
-    .list {
-      height: 50px;
-      line-height: 50px;
-      padding-left: 10px;
-      margin-left: 20px;
-      width: 100%;
-      border-bottom: solid #E9E9E9 1px;
-    }
-
-
-    .edit-panel {
-      width: 45%;
-      height: 100%;
-    }
-
-    #txt-area {
-        width: 100%;
-        height: 100%;
-        background-color: #333333;
-        color: #FFFFFF;
-        font-size: 14px;
-        resize: none;
-    }
-
-    .show-panel {
-      width: 40%;
-      margin-top: 0;
-      padding-top: 0;
-      margin-left: 5px;
-      padding-left: 5px;
-    }
-    .selected {
-      background-color: #42b983;
-    }
+ @import "assets/css/babel.css";
 </style>
